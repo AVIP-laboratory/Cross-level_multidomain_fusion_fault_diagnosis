e9f48f3ea160782476b1c596ac3e41fcf8771403
AVIP-laboratory <avip00316@gmail.com>
2025-03-17 09:17:52 +0900

    Update Model A.py

--- Codes/Model A.py
+++ Codes/Model A.py
@@ -287,7 +287,7 @@ if job == 1:
 
     #train the model
     history = model.fit([Time_data_V, Time_data_C1, Time_data_C2, Time_data_C3], Y_train,
-                         epochs=10,
+                         epochs=50,
                          batch_size=32,
                          validation_split=0.1,
                          validation_steps=4)
